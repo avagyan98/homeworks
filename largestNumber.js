20fa2b4723a8ed21fbc8329192f4ccce422599d6
yeghishe98 <yeghishe.avagyan@picsart.com>
2023-01-17 12:24:59 +0400

    fixed comments

--- largestNumber.js
+++ largestNumber.js
@@ -1,13 +1,28 @@
+const arr = [3, 5, 1, 37, 26];
+
 function largest(arr) {
     return Math.max(...arr)
 }
 
-console.log(largest([3, 5, 1, 37, 26]))
+console.log(largest(arr))
 
 
 function largest2(arr) {
-    arr.sort();
-    return arr[-1]
+    arr.sort((a, b) => a - b);
+    return arr[arr.length - 1]
 }
 
-console.log(largest2([3, 5, 1, 37, 26]))
+console.log(largest2(arr))
+
+
+function largest3(arr) {
+    maxNumber = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        if(maxNumber < arr[i]) {
+            maxNumber = arr[i];
+        }
+    }
+    return maxNumber
+}
+
+console.log(largest3(arr))
--- product.js
+++ product.js
@@ -1,13 +1,13 @@
-function product(arr) {
+const arr = [3, 7, -13, -4];
+
+function productMultiply(arr) {
     let negativeCount = 0;
+
+    if(arr.includes(0)) return 'The product is 0';
+
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 0) {
-            return 'The product is 0'
-        } else if (arr[i] < 0) {
-            negativeCount ++;
-            continue
-        } else {
-            continue
+        if (arr[i] < 0) {
+            negativeCount++;
         }
     }
     if (negativeCount % 2 === 0) {
@@ -17,4 +17,25 @@ function product(arr) {
     }
 }
 
-console.log(product([3, 7, -13, -4]));
+console.log(productMultiply(arr));
+
+
+function productAdd(arr) {
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum)
+
+    if(sum > 0) {
+        return 'The product is positive'
+    }else if(sum < 0) {
+        return 'The product is negative'
+    }else{
+        return 'The product is 0';
+    }
+}
+
+console.log(productAdd(arr));
+
