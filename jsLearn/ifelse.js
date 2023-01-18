6dfde0ea048bf325572c56e5ea1879c30e6ac3d6
yeghishe98 <yeghishe.avagyan@picsart.com>
2023-01-18 19:50:50 +0400

    logical_operators

---
+++ jsLearn/ifelse.js
@@ -0,0 +1,37 @@
+// 1.
+
+let name = prompt("What is the official name of JavaScript?", 'JS');
+
+if(name == "ECMAScript") {
+    alert("True");
+}else {
+    alert("Do not know? ECMAScript!");
+}
+
+
+// 2.
+
+let number = prompt("write number", 100);
+
+if(number > 0) {
+    alert(1)
+}else if(number < 0) {
+    alert(-1)
+}else {
+    alert(0)
+}
+
+
+// 3.
+
+let resalt = (a + b < 4) ? "few" : "many";
+
+
+// 4.
+
+let massige = login == "Employee"
+    ? "Hey" : login == "Director"
+    ? "Hello" : login == ''
+    ? "No login" : "";
+
+
---
+++ jsLearn/logicalOperators.js
@@ -0,0 +1,28 @@
+// 1.
+
+let age = prompt("write age", 20);
+let result = age >= 14 && age <= 90 ? true : false;
+
+
+// 2
+
+let age = prompt("write age", 20);
+let result = !(age >= 14 && age <= 90) ? true : false;
+let result1 = age < 14 && age >90 ? true : false;
+
+
+// 3.
+
+let login = prompt("Who is this?", "name");
+
+if(login == "Admin"){
+    let parol = prompt("Parol", "");
+
+    if(parol == "I'm in charge") {
+        alert("Hello");
+    }else {
+        alert("Wrong password");
+    }
+}else {
+    alert("I don't know you");
+}
