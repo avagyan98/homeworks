5b8e443377a18c0d5e3ac225b72879253a94fd54
yeghishe98 <yeghishe.avagyan@picsart.com>
2023-04-12 18:19:23 +0400

    fixed comments

--- strings.js
+++ strings.js
@@ -1,33 +1,27 @@
 // 1. Uppercase the first character
 
 function ucFirst(str) {
-    str = str[0].toUpperCase() + str.slice(1);
+    str = str[0].toUpperCase() + str.slice(1).toLowerCase();
     return str
 }
 
-let text = 'text';
+let text = 'teXt';
 console.log(ucFirst(text));
 
 // 2. Check for spam
 
-function checkSpam(str, checkStr1, checkStr2) {
+function checkSpam(str) {
     str = str.toLowerCase();
-    checkStr1 = checkStr1.toLowerCase();
-    checkStr2 = checkStr2.toLowerCase();
 
-    if(str.includes(checkStr1) || str.includes(checkStr2)){
-        return true
-    }
-
-    return false
+    return str.includes('viagra'.toLowerCase()) || str.includes('XXX'.toLowerCase())
 }
 
-console.log(checkSpam("asdsd sdf viagra", 'viaGra', 'XXX'));
+console.log(checkSpam("asdsd sdf viAgra"));
 
 // 3. Truncate the text
 
 function truncate(str, lengthStr) {
-    return (str.length > lengthStr) ? str.slice(0, lengthStr - 3) + "..." : str
+    return (str.length > lengthStr) ? `${str.slice(0, lengthStr - 3)}...` : str
 }
 
 console.log(truncate("Hi everyone!", 10))
@@ -35,17 +29,9 @@ console.log(truncate("Hi everyone!", 10))
 // 4. Extract the money
 
 function extractCurrencyValue(str) {
-    let firstNumber;
-    for(let i = 0; i < str.length; i++){
-        if (str.codePointAt(i) >= 48 && str.codePointAt(i) <= 57) {
-            firstNumber = i;
-            break
-        }
-    }
-
-    return Number(str.slice(firstNumber))
+    return Number(str.slice(1))
 }
 
-const many = "$$340"
+const money = "$340"
 
-console.log(extractCurrencyValue(many))
+console.log(extractCurrencyValue(money))
